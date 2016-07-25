8500c2ad3b743e9db8f3525b504a46f92c657302
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-07-25 14:54:42 -0400

    Spin me

--- PickinFruit/ViewController.swift
+++ PickinFruit/ViewController.swift
@@ -8,26 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate  {
+    
     @IBOutlet weak var spinButton: UIButton!
     @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var fruitPicker: UIPickerView!
     
     var fruitsArray = ["🍎", "🍊", "🍌", "🍐", "🍇", "🍉", "🍓", "🍒", "🍍"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // self.fruitPicker.accessibilityLabel = Constants.FRUIT_PICKER
+        self.fruitPicker.accessibilityLabel = Constants.FRUIT_PICKER
         self.spinButton.accessibilityLabel = Constants.SPIN_BUTTON
+        self.fruitPicker.delegate = self
+        self.fruitPicker.dataSource = self
         
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return self.fruitsArray.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        let title = self.fruitsArray[row]
+        return title
     }
     
     @IBAction func spinButtonTapped(sender: UIButton) {
         
-        
+        for i in 0...2 {
+            
+            self.fruitPicker.selectRow(Int(arc4random_uniform(9)), inComponent: i, animated: true)
+            
+            if self.fruitPicker.selectedRowInComponent(0) == self.fruitPicker.selectedRowInComponent(1) && self.fruitPicker.selectedRowInComponent(1) == self.fruitPicker.selectedRowInComponent(2) {
+                self.resultLabel.text = "WINNER!"
+            } else {
+                self.resultLabel.text = "TRY AGAIN"
+            }
+        }
     }
-    
 }
 
 // MARK: Set Up
